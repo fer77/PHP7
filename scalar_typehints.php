2a4b847548332de191be674bf4bd2317240304dd
Fernando Fernandez <ffmp777@yahoo.com>
2017-03-23 08:41:49 -0600

    first commit

---
+++ scalar_typehints.php
@@ -0,0 +1,19 @@
+<?php 
+// declare(strict_types=1);
+
+//Scalar Typehints: 
+function setAge(int $age)
+{
+	# code...
+}
+
+setAge(30); // Not entirely strict, int will accept something like setAge('30').  To make scalar typehints strict add: declare(strict_types=1); at the top.
+
+function setIsValid(bool $valid)
+{
+	var_dump($valid);
+}
+
+setIsValid(true);  //As always 1 is true -1 and 0 return false.  A 'string' is turned into a bool.  Objects and arrays are not coherced.
+setIsValid('some string');
+ ?>
