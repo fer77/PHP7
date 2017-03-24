4f96aaedda813c840acd88dd6e99661c16ad2f13
Fernando Fernandez <ffmp777@yahoo.com>
2017-03-24 08:48:13 -0600

    'spaceship operator'

--- index.php
+++ index.php
@@ -1,39 +1,84 @@
-<?php 
+<?php
 
-// class User {}
+//* Combined comparison operator <=>, can determine the proper sort order.
 
-// function getUser()
-// {
-// 	return new User;  //* there's no protection against returning an array or something else.
-// 	return [];
-// }
+$belchers = ['Bob', 'Linda', 'Tina', 'Gene', 'Louise'];
 
-//* Typend returned values: makes things a little more strict.  Only do this in situations where it is very important where people that user an interface adhere to a "contarct".
-// class User {}
-
-// function getUser() : User
-// {
-// 	return []; //* This will not be allowed.
-// 	return new User;
-// }
-
-// var_dump(getUser());
-
-//* This can be useful for defining interfaces where others will create the concrete implementations.
-class User{}
-
-interface SomeInterface
+//* Sort them in alphabetical order (this is not new to PHP7).
+sort($belchers);
+//* Combined comparison operator <=>
+usort($belchers, function ($a, $b)
 {
-	public function getUser() : User;
-}
+	// var_dump('a: ' . $a . ', ' . 'b: ' . $b);
+	return $a <=> $b; //* will either return -1, 0, or 1
+	// If $a is less than $b return -1
+	// If $a is equal to $b return 0
+	// If $a is greater than $b return 1
+});
 
-class SomeClass implements SomeInterface
+//* Sort in Reverse (this is not new to PHP7).
+rsort($belchers);
+//* Combined comparison operator <=>
+usort($belchers, function ($a, $b)
 {
-	public function getUser() : User
-	{
-		return new User;
+	return $b <=> $a;
+});
+
+//* Sort by charachter length.
+usort($belchers, function ($a, $b)
+{
+	//var_dump('a: ' . $a . ', ' . 'b: ' . $b);
+	return strlen($a) <=> strlen($b);
+});
+
+//* Sort by charachter length (reverse order).
+usort($belchers, function ($a, $b)
+{
+	//var_dump('a: ' . $a . ', ' . 'b: ' . $b);
+	return strlen($b) <=> strlen($a);
+});
+class User {
+	protected $name;
+	protected $age;
+
+	public function __construct($name, $age) {
+		$this->name = $name;
+		$this->age = $age;
+	}
+	public function name() {
+		return $this->name;
+	}
+	public function age() {
+		return $this->age;
 	}
 }
 
-(new SomeClass)->getUser();
+class UserCollection {
+	protected $users;
+	public function __construct(array $users)
+	{
+		$this->users = $users;
+	}
+	public function users() {
+		return $this->users;
+	}
+	//* Sort this collection by name, age...
+	public function sortBy($method) {
+		usort($this->users, function($userOne, $userTwo) use ($method) {
+			return $userOne->$method() <=> $userTwo->$method();
+		});
+	}
+}
+
+$collection = new UserCollection([
+	new User('Bob', 41),
+	new User('Linda', 40),
+	new User('Tina', 13),
+	new User('Gene', 11),
+	new User('Louise', 9)
+	]);
+
+//$collection->sortBy('name');
+$collection->sortBy('age');
+var_dump($collection->users());
  ?>
