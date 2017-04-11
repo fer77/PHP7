1f0ea8bfedf95e862fa858ba22bbb2e131189a26
Fernando <ffmp777@yahoo.com>
2017-04-10 19:56:46 -0600

    cleaned up code

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 
-//* Null Coalesce Operator ??
-
-//$_GET['name'] = 'Bob';
-
-//$name = isset($_GET['name']) ? $_GET['name'] : 'nothing'; //* This is a lot to write.
-
-//* ??
-//$name = $_GET['name'] ?? 'nothing';
-
-//var_dump($name);
-
-$name = "Gene Belcher";
-
-echo $name ?? 'Bob Belcher';
 
 
 
